090c5cec3b857ac642a0dd380e34e57108763f00
nazarchykur <nazar.chykur@gmail.com>
2023-01-31 14:54:09 +0200

    added test

--- src/test/java/com/example/sbjenkins/SbJenkinsApplicationTests.java
+++ src/test/java/com/example/sbjenkins/SbJenkinsApplicationTests.java
@@ -3,11 +3,14 @@ package com.example.sbjenkins;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class SbJenkinsApplicationTests {
 
     @Test
     void contextLoads() {
+        assertThat(true).isEqualTo(true);
     }
 
 }
