0a99ccb371cc9391c95f19cafef9a293a63b1cdf
nazarchykur <nazar.chykur@gmail.com>
2023-01-31 13:20:22 +0200

    init

---
+++ src/main/java/com/example/sbjenkins/SbJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbJenkinsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sbjenkins/SbJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
